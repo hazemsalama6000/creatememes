507ab6b78d2ecbee2d46bb0392ae88bcbc0343b1
Your Name <hazem.salama60@yahoo.com>
2022-10-21 16:53:44 +0200

    create and design header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Generator } from './generator/generator.component';
+import { Navbar } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Generator,
+    Navbar
   ],
   imports: [
     BrowserModule,
